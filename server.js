fa4b12220e1d9e8df5473c6cab38cdc25c870a82
jlp0328 <jlp0328@gmail.com>
2017-05-03 23:16:06 -0400

    Server.js filed updated, MVC updated, handlebars added, icons added, additional packages installed

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var expressValidator = require("express-validator");
+var flash = require("connect-flash");
 var session = require("express-session");
 var passport = require("passport");
+var exphbs = require("express-handlebars");
 
+var mongoose = require("mongoose");
+
+
+var LocalStrategy = require("passport-local").Strategy;
 var FacebookStrategy = require("passport-facebook");
 var MongoStore = require("connect-mongo")(session);
 
@@ -13,23 +19,68 @@ mongoose.Promise = Promise;
 var Dater = require("./models/Dater");
 var User = require("./models/User");
 
+var routes = require("./routes/index");
+var local = require("./routes/local");
 
+//Express
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static(process.cwd() + "/public"));
 
+//Body-Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+//Express-Session
+app.use(session({
+  secret: "secret",
+  saveUninitialized: true,
+  resave: true
 
-app.use(session);
+  }));
 
+//Express-Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Connect Flash
 app.use(flash());
 
+app.use(function(req, res, next){
+
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+
+});
+
+app.use("/", routes);
+app.use("/local", local);
+
+
+//Passport Initialization
 app.use(passport.initialize());
 
 app.use(passport.session());
@@ -55,6 +106,8 @@ db.once("open", function() {
 app.listen(port, function() {
   console.log("App running on port " + port);
 });
+
+
 
 //Route to root and connect index.html
 app.get("/", function(req, res) {
