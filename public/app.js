2ec817dedeec25cec8c128438d483d7f39db0170
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-05-06 09:02:32 -0400

    working on scraping to db

---
+++ public/app.js
@@ -0,0 +1,19 @@
+//findDaterButton will id of button to search for dater on Okcupid
+$(document.on("click", "#findDaterButton"), function(e) {
+	e.preventDefault();
+	var daterName = $(this).text(".searchDater");
+	//need to see if we will have a data-id
+	var thisId = $(this).attr("data-id");
+
+	$.ajax({
+		method: "POST",
+		url: "/dateScrape" + thisId,
+		data: {
+			username: daterName
+		}
+	}).done(function(data) {
+		//clear input field for dater
+		// $(".searchDater").empty();
+	});
+
+});
